4f9c9aa04d2634b884ca8cc2abce182a6413a512
Qasim Naseer <qasimnaseer@gsoftconsulting.com>
2023-08-18 16:48:29 +0500

    test added

---
+++ cypress/support/pageObjects/desktop/testenv.cy.js
@@ -0,0 +1,8 @@
+describe('',()=>{
+    it('',()=>{
+        cy.log(Cypress.env('baseURL'));
+        cy.log(Cypress.env('email'));
+        cy.log(Cypress.env('password'));
+        cy.log(Cypress.env('apiURL'));
+    });
+});
