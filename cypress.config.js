2d316af9ce08358ec17ca66b113985421511d0e7
Qasim Naseer <qasimnaseer@gsoftconsulting.com>
2023-08-21 11:05:18 +0500

    screenshots, package

--- cypress.config.js
+++ cypress.config.js
@@ -8,9 +8,20 @@ module.exports = defineConfig({
     specPattern: "**/*.cy.js",
     reporter: "cypress-multi-reporters",
     screenshotOnRunFailure: true,
+    screenshotFolder:cypress/screenshots,
     video: false,
     projectId: 'xy9pnf',//128076ed-9868-4e98-9cef-98dd8b705d75,
     defaultCommandTimeout : 10000,
+    
+      reporter: "mochawesome",
+      reporterOptions: {
+          reportDir: "cypress/report/mochawesome-report",
+          reportFilename: "[name]-report",
+          overwrite: true,
+          htm: true,
+          json: false,
+          timestamp: "mmddyyyy_HHMMss",
+      },
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
